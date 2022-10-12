a5e1b4137cd6c2f11770f9521279dec8dda70684
Mike Thomas <mikethomas84@gmail.com>
2022-10-12 17:59:22 +0100

    product model & factory

--- tests/Feature/SearchTest.php
+++ tests/Feature/SearchTest.php
@@ -3,9 +3,23 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use App\Models\Product;
 
 
 class SearchTest extends TestCase
 {
+    /** @test */
+    public function food_search_page_is_accessible()
+    {
+        $this->get('/')
+            ->assertOk();
+    }
+
+    /** @test */
+    public function food_search_page_has_all_the_required_page_data()
+    {
+        // Arrange phase
+        Product::factory()->count(3)->create(); // create 3 products
+    }
 
 }
